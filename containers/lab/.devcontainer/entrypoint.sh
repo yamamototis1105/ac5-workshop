90495634d8fb2c2d9657bf136fe179c01058057a
yamamototis1105 <yamamototis1105@gmail.com>
2026-06-08 10:43:20 +0000

    update

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -16,3 +16,8 @@ if ! ${CODESPACES:-false}; then
         fi
     fi
 fi
+
+if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
+    CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
+fi
+code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
